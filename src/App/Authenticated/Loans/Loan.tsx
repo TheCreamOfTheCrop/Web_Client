d2dc5c13dcb81dbfa1aed97b13cb6b877f670cfe
yohan fairfort <yohan.fairfort@gmail.com>
2018-05-22 17:45:00 +0200

    Add User information on loans

--- src/App/Authenticated/Loans/Loan.tsx
+++ src/App/Authenticated/Loans/Loan.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Col, Panel } from 'react-bootstrap';
+import { Col, Panel, Label } from 'react-bootstrap';
 import ILoan from './ILoan';
-// import { postWithPayload } from '../post';
+import { postWithPayload } from '../post';
 
 import { Button, Row } from 'react-bootstrap';
 class Loan extends React.Component<any, any> {
@@ -16,17 +16,20 @@ class Loan extends React.Component<any, any> {
         };
     }
     componentDidMount() {
-        // postWithPayload('http://' + process.env.REACT_APP_BMB_API + '/user', 
-        // { id: this.props.loan.user_provider_id })
-        // .then((res: any) => {
-        //     this.setState({user : res});
-        // });
+        postWithPayload('http://' + process.env.REACT_APP_BMB_API + '/user', 
+                        { id: this.props.loan.id})
+        .then((res: any) => {
+            this.setState({user : res.user});
+        });
     }
     showDetails() {
         // TODO: plug to a true show details
     }
     render() {
         return(
+            this.state.user === undefined ? 
+            null :
+            (
             <Col md={4}>
                 <Panel>
                     <Panel.Heading>
@@ -41,12 +44,15 @@ class Loan extends React.Component<any, any> {
                                 <Button type="button" onClick={this.showDetails}>Details</Button>
                             </Col>
                             <Col md={3} mdOffset={3}>
-                                {this.props.loan.amount} €
+                                <h4>
+                                    <Label>{this.props.loan.amount} €</Label>
+                                </h4>
                             </Col>
                         </Row>
                     </Panel.Footer>
                 </Panel>
             </Col>
+            )
         );
     }
 }
